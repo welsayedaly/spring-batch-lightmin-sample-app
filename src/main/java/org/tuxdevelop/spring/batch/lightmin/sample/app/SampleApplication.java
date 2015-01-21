46d928a9bbc0053c1513ac0cdc9e9e77b4590ad9
tuxdevelop <tuxdevelop@gmail.com>
2015-01-21 11:34:51 +0100

    refactored configuration

--- src/main/java/org/tuxdevelop/spring/batch/lightmin/sample/app/SampleApplication.java
+++ src/main/java/org/tuxdevelop/spring/batch/lightmin/sample/app/SampleApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.tuxdevelop.spring.batch.lightmin.configuration.AbstractSpringBatchLightminConfiguration;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "org.tuxdevelop.spring.batch.lightmin")
-public class SampleApplication {
+@ComponentScan
+public class SampleApplication extends AbstractSpringBatchLightminConfiguration{
 
 	public static void main(final String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
--- src/main/java/org/tuxdevelop/spring/batch/lightmin/sample/app/configuration/ApplicationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.tuxdevelop.spring.batch.lightmin.sample.app.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.tuxdevelop.spring.batch.lightmin.configuration.AbstractSpringBatchLightminConfiguration;
-
-@Configuration
-public class ApplicationConfiguration extends AbstractSpringBatchLightminConfiguration {
-
-}
